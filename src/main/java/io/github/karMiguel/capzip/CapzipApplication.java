d776c28eae676a8d5b0861ab4b93427d10d476bf
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-06-04 16:33:52 -0300

    feat :tada: Commit inicial

---
+++ src/main/java/io/github/karMiguel/capzip/CapzipApplication.java
@@ -0,0 +1,15 @@
+package io.github.karMiguel.capzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+
+public class CapzipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapzipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/karMiguel/capzip/CapzipApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.karMiguel.capzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapzipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
