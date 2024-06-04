9db46137f82c4cec9d12ca1b9f707d992719d334
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-06-04 17:04:49 -0300

    feat :sparkles: config api docs swagger

---
+++ src/main/java/io/github/karMiguel/capzip/config/OpenApiConfig.java
@@ -0,0 +1,41 @@
+package io.github.karMiguel.capzip.config;
+
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("RESTful API with Java 21 and Spring Boot 3.2.0")
+                        .version("v1")
+                        .description("Some description about your API")
+                        .termsOfService("https://github.com/KarMiguel")
+                        .license(
+                                new License()
+                                        .name("Apache 2.0")
+                                        .url("https://github.com/KarMiguel")
+                        )
+                ).components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth", Collections.emptyList()));
+    }
+
+}
