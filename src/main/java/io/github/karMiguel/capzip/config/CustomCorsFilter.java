a3ee26d9f768bd093a61603bdc776789ee1dac2d
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-06-18 16:06:17 -0300

    feat :sparkles: config projetct redirect short link

---
+++ src/main/java/io/github/karMiguel/capzip/config/CustomCorsFilter.java
@@ -0,0 +1,46 @@
+package io.github.karMiguel.capzip.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomCorsFilter implements Filter {
+
+    @Override
+    public void doFilter(jakarta.servlet.ServletRequest request, jakarta.servlet.ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        String origin = req.getHeader("Origin");
+
+        // Allow all origins or customize based on the request origin
+        res.setHeader("Access-Control-Allow-Origin", origin);
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
+            res.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // No initialization needed
+    }
+
+
+    @Override
+    public void destroy() {
+        // No cleanup needed
+    }
+}
