7eb8d11956b975ba5e960625eda06b1362c7ed03
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-06-18 16:06:28 -0300

    feat :sparkles: main

--- src/main/java/io/github/karMiguel/capzip/CapzipApplication.java
+++ src/main/java/io/github/karMiguel/capzip/CapzipApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-
 public class CapzipApplication {
 
 	public static void main(String[] args) {
