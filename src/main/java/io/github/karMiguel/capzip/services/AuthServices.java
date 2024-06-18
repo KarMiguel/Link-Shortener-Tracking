37bee52c01464ef4ccd9c8278bfce48acbca581e
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-06-18 16:03:58 -0300

    feat :sparkles: services auth

--- src/main/java/io/github/karMiguel/capzip/services/AuthServices.java
+++ src/main/java/io/github/karMiguel/capzip/services/AuthServices.java
@@ -2,19 +2,19 @@ package io.github.karMiguel.capzip.services;
 
 import io.github.karMiguel.capzip.dtos.AccountCredentialsDto;
 import io.github.karMiguel.capzip.dtos.TokenDto;
-import io.github.karMiguel.capzip.model.User;
+import io.github.karMiguel.capzip.exceptions.InvalidJwtAuthenticationException;
+import io.github.karMiguel.capzip.model.Users;
 import io.github.karMiguel.capzip.repository.UserRepository;
 import io.github.karMiguel.capzip.security.JwtTokenProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.logging.Logger;
 
 @Service
 @Slf4j
@@ -30,37 +30,31 @@ public class AuthServices {
     private UserRepository repository;
 
     @SuppressWarnings("rawtypes")
-    public ResponseEntity signin(AccountCredentialsDto data) {
-        try {
-            var username = data.getEmail();
-            var password = data.getPassword();
-            log.info("comecei");
+    public Object signin(AccountCredentialsDto data) {
+        var username = data.getEmail();
+        var password = data.getPassword();
+        log.info("Iniciando autenticação para o usuário: {}", username);
 
+        try {
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(username, password));
 
-            log.info("passei aqui 1");
-            User user = repository.findByEmail(username).orElseThrow(
-                    ()-> new UsernameNotFoundException("Username not found!")
-            );
-            log.info("passei aqui 2");
+            Users user = repository.findByEmail(username)
+                    .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + username));
 
-            var tokenResponse = new TokenDto();
-            if (user != null) {
-                log.info("passei aqui 3 - antes token");
-
-                tokenResponse = tokenProvider.createAccessToken(username, user.getRole());
-                log.info("passei aqui 4 - depois token");
-
-            } else {
-                throw new UsernameNotFoundException("Username " + username + " not found!");
-            }
-            log.info("finalizei aquii");
-            return ResponseEntity.ok(tokenResponse);
-        } catch (Exception e) {
-            throw new BadCredentialsException("Invalid username/password supplied!");
+            // Criar e retornar o token de acesso
+            return tokenProvider.createAccessToken(username, user.getRole());
+        } catch (UsernameNotFoundException ex) {
+            log.error("Usuário não encontrado: {}", ex.getMessage());
+            throw new InvalidJwtAuthenticationException("Credenciais inválidas: " + ex.getMessage());
+        } catch (BadCredentialsException ex) {
+            log.error("Credenciais inválidas: {}", ex.getMessage());
+            throw new InvalidJwtAuthenticationException("Credenciais inválidas: " + ex.getMessage());
         }
     }
+
+
+
 
     @SuppressWarnings("rawtypes")
     public ResponseEntity refreshToken(String username, String refreshToken) {
