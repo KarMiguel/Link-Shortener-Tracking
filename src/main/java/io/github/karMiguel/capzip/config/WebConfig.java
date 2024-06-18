32e978653a6e2d9d84dc88126aea945d3cae77f1
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-06-18 16:01:48 -0300

    feat :sparkles: CORS

--- src/main/java/io/github/karMiguel/capzip/config/WebConfig.java
+++ src/main/java/io/github/karMiguel/capzip/config/WebConfig.java
@@ -2,11 +2,15 @@ package io.github.karMiguel.capzip.config;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.UrlPathHelper;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
@@ -17,7 +21,14 @@ public class WebConfig implements WebMvcConfigurer{
     private String corsOriginPatterns = "";
 
 
+//    @Autowired
+  //  private LinkShortInterceptor linkShortInterceptor;
+
     @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //registry.addInterceptor(linkShortInterceptor);
+    }
+   /* @Override
     public void addCorsMappings(CorsRegistry registry) {
         var allowedOrigins = corsOriginPatterns.split(",");
         registry.addMapping("/**")
@@ -25,5 +36,11 @@ public class WebConfig implements WebMvcConfigurer{
                 .allowedMethods("*")
                 .allowedOrigins(allowedOrigins)
                 .allowCredentials(true);
+    }*/
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        UrlPathHelper urlPathHelper = new UrlPathHelper();
+        urlPathHelper.setUrlDecode(false);
+        configurer.setUrlPathHelper(urlPathHelper);
     }
 }
