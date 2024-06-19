bec0d6316304033425a42253a58abe091c53b9c8
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-06-19 11:41:39 -0300

    feat :sparkles: created totalDto click e link

---
+++ src/main/java/io/github/karMiguel/capzip/dtos/TotalDto.java
@@ -0,0 +1,17 @@
+package io.github.karMiguel.capzip.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TotalDto {
+
+    private int total;
+
+}
