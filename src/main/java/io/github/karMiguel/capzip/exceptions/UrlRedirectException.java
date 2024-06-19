094f241fa7c8071b5ff3f6ee4851ff6b11982cdc
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-06-19 11:42:40 -0300

    feat :sparkles: created exception url

---
+++ src/main/java/io/github/karMiguel/capzip/exceptions/UrlRedirectException.java
@@ -0,0 +1,7 @@
+package io.github.karMiguel.capzip.exceptions;
+
+public class UrlRedirectException extends RuntimeException {
+    public UrlRedirectException(String message) {
+        super(message);
+    }
+}
